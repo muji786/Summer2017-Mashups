c49b1bbffde4d1c1bfcf3ad2783e591ef706872b
Ahmad Arshad <muji786@users.noreply.github.com>
2017-08-02 16:16:10 -0400

    update

--- Assisgnment July 25 2017/script.js
+++ Assisgnment July 25 2017/script.js
@@ -1,59 +1,81 @@
 var app = {
-	
-	itpURL: "https://itp.nyu.edu/ranch/api/itp-yearbook/",
+	itpYearBook: "https://itp.nyu.edu/ranch/api/itp-yearbook/",
+	itpProjectsbyYear: "https://itp.nyu.edu/ranch/api/projects-finder-by-class/",
+	itpProjectsperStudent: "https://itp.nyu.edu/ranch/api/projects-finder-by-creator/",
+
+	studentsPerYear: null,
+	projectsPerYear: null,
+	projectsPerStudent: null,
 
 	initialize: function() {
 		$("#search").click(function(){
-			console.log("Clicked search");
 			$("#resultsTarget").html("");
 			var newSearchTerm = $("#query").val();
-			console.log(newSearchTerm);
-			app.itpSearch(newSearchTerm);
+			currentYear = new Date().getFullYear() + 2;
+			if (newSearchTerm == "" || newSearchTerm < 1981 || newSearchTerm > currentYear)
+			{
+				newSearchTerm = currentYear;
+			}
+			app.itpYearBookSearch(newSearchTerm);
+			app.itpProjectsbyYearSearch(newSearchTerm);
 		});
 
 		$("#query").keypress(function(e){
 			if (e.which == 13){
-				$("#search").trigger('click');
+				$("#search").trigger('click')
 			}
 		});
 	},
 
-	itpSearch: function(searchTerm) {
-		currentYear = new Date().getFullYear() + 2;
-		if (searchTerm == "" || searchTerm < 1981 || searchTerm > currentYear)
-		{
-			searchTerm = currentYear;
-		}
+	itpYearBookSearch: function(searchTerm) {
 		$.ajax({
-			url: this.itpURL + searchTerm,
+			url: this.itpYearBook + searchTerm,
 			type: 'GET',
 			dataType: 'json',
 			error: function(data){
-				console.log("We got problems");
-				//console.log(data.status);
+				$("#searchTerm").html(data);
 			},
 			success: function(data){
-				console.log("WooHoo!");
-				console.log(data);
-				$("#searchTerm").html("Class of " + searchTerm);
+					studentsPerYear = data;
+					$("#searchTerm").html("Class of " + searchTerm + " : " + studentsPerYear.length + " students.");
 
 					var htmlString = '';
-					var len = data.length;
-					var urlResults = data[3];
-				//Loop through the array of results
-					for (var i = 0; i < len; i++){
+					for (var i = 0; i < studentsPerYear.length; i++){
 					htmlString =	"<p class='itpResults'>" +
-											data[i].name +
+											studentsPerYear[i].name +
 										"</p>";
 					$("#resultsTarget").append(htmlString);
+					 //app.itpProjectsperStudentSearch(studentsPerYear[i].netid);
 				}
+			}
+		});
+	},
+
+	itpProjectsbyYearSearch: function(searchItem){
+		$.ajax({
+			url: this.itpProjectsbyYear + searchTerm,
+			type: 'GET',
+			dataType: 'json',
+			error: function(data){
+				$("#searchTerm").html(data);
+			},
+			success: function(data){
+				projectsPerYear = data;
+			}
+		});
+	},
+
+	itpProjectsperStudentSearch: function(searchItem){
+		$.ajax({
+			url: this.itpProjectsperStudent + searchItem,
+			type: 'GET',
+			dataType: 'json',
+			error: function(data){
+				$("#searchTerm").html(data);
+			},
+			success: function(data){
+				projectsPerStudent = data;
 			}
 		});
 	}
 }
-
-//Code to be executed once the page has fully loaded
-$(document).ready(function(){
-		console.log("LOADED!!!!");
-	app.initialize();
-});
