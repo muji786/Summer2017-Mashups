778fad00d0f7244d2174e697309909374f701b70
Ahmad Arshad <muji786@users.noreply.github.com>
2017-08-08 17:44:06 -0400

    update

--- Project1/script.js
+++ Project1/script.js
@@ -3,13 +3,14 @@ var app = {
     itpProjectsbyYear: "https://itp.nyu.edu/ranch/api/projects-finder-by-class/",
     itpProjectsperStudent: "https://itp.nyu.edu/ranch/api/projects-finder-by-creator/",
     itpProjects: "https://itp.nyu.edu/ranch/api/projects/",
-    giphySearch: "https://api.giphy.com/v1/gifs/search?api_key=f83e46e2508b41bd954c4b821e8261ea&limit=1&offset=0&rating=G&lang=en&q=",
+    giphySearch: "https://api.giphy.com/v1/gifs/search?api_key=f83e46e2508b41bd954c4b821e8261ea&limit=25&offset=0&rating=G&lang=en&q=",
+    //giphySearch: "https://api.giphy.com/v1/gifs/random?api_key=f83e46e2508b41bd954c4b821e8261ea&rating=G&tag=",
 
     studentsPerYear: [],
     projectsPerYear: [],
     projectsPerStudent: [],
     projectDetails: [],
-    giphy: null,
+    giphyURL: "itp.png",
 
     initialize: function () {
         $("#search").click(function () {
@@ -30,22 +31,59 @@ var app = {
         });
 
         $("#resultsTarget").on("click", ".itpResults", function () {
-            var id = $(this).prop("id");
-            var newSearchTerm = $($(id).text());
+            var newSearchTerm = $('#' + this.id).html();
             console.log(newSearchTerm);
-            app.runGiphySearch(newSearchTerm);
-            //            app.itpProjectsperStudentSearch(studentsPerYear[id].netid);
-            //            if (projectsPerStudent.length > 0 && projectsPerStudent != null) {
-            //                for (var i = 0; i < projectsPerStudent.length; i++) {
-            //                    app.itpProjectDetails(projectsPerStudent[i].id);
-            //                    htmlString = "<div class='projectDetails' " + "id ='Project" + i + "'>" + projectDetails[0].name +
-            //                        "</div>";
-            //                    $(this).append(htmlString);
-            //                    console.log("ProjectDetails" + projectDetails);
-            //                        }
-            //}
+
+            giphyURL = "itp.png";
+            $.ajax({
+                url: app.giphySearch + newSearchTerm,
+                type: 'GET',
+                dataType: 'json',
+                async: false,
+                error: function (data) {
+                    $("#searchTerm").html(data);
+                },
+                success: function (data) {
+                    console.log(data.data.length);
+                    if (data.data.length > 0) {
+                        giphyURL = data.data[Math.floor(Math.random() * data.data.length)].images.original.url;
+                        console.log(giphyURL);
+                    }
+                }
+            });
+            var img = $("<img />").attr('src', giphyURL).on('load', function () {});
+            $('#' + this.id).append(img);
         });
     },
+
+    //        $("#resultsTarget").on("click", ".itpResults", function () {
+    //            console.log("myid:" + this.id);
+    //            var newSearchTerm = $('#' + this.id).html();
+    //            app.runGiphySearch(newSearchTerm, this.id);
+    //            $('#' + this.id).append(img);
+    //            //console.log($('#' + this.id).append(img));
+    //            // console.log($('#' + id + "giphy").html(giphyURL));
+    //            //var htmlString = "<div class='giphyResults' id ='" + id + "giphy>" + giphyURL + "</div>";
+    //            //            $('#' + this.id).append('<img src="' + giphyURL + '">');
+    //            //            console.log('<img src="' + giphyURL + '">');
+    //            //            $('#' + this.id).load(giphyURL);
+    //            //            $('<img src="' + giphyURL + '">').load(function () {
+    //            //                $(this).appendTo('#' + this.id);
+    //            //            });
+    //            //$('#' + this.id).append(giphyURL);
+    //
+    //            //            app.itpProjectsperStudentSearch(studentsPerYear[id].netid);
+    //            //            if (projectsPerStudent.length > 0 && projectsPerStudent != null) {
+    //            //                for (var i = 0; i < projectsPerStudent.length; i++) {
+    //            //                    app.itpProjectDetails(projectsPerStudent[i].id);
+    //            //                    htmlString = "<div class='projectDetails' " + "id ='Project" + i + "'>" + projectDetails[0].name +
+    //            //                        "</div>";
+    //            //                    $(this).append(htmlString);
+    //            //                    console.log("ProjectDetails" + projectDetails);
+    //            //                        }
+    //            //}
+    //        });
+    //    },
 
     itpYearBookSearch: function (searchTerm) {
         $.ajax({
@@ -61,12 +99,10 @@ var app = {
 
                 var htmlString = '';
                 for (var i = 0; i < studentsPerYear.length; i++) {
-                    htmlString = "<div class='itpResults' " + "id ='" + i + "result'>" +
-                        studentsPerYear[i].name +
-                        "</div>";
-                    $("#resultsTarget").append(htmlString);
+                    htmlString += "<div class='itpResults' id ='" + i + "result'>" + studentsPerYear[i].name + "</div>";
                     //app.itpProjectsperStudentSearch(studentsPerYear[i].netid);
                 }
+                $("#resultsTarget").append(htmlString);
             }
         });
     },
@@ -112,18 +148,4 @@ var app = {
             }
         });
     },
-
-    runGiphySearch: function (searchItem) {
-        $.ajax({
-            url: this.giphySearch + searchItem,
-            type: 'GET',
-            dataType: 'json',
-            error: function (data) {
-                $("#searchTerm").html(data);
-            },
-            success: function (data) {
-                console.log(data);
-            }
-        });
-    }
 }
